dfb3f614896aced015315125b66b1a40d4550c1a
Ellie Judge <judge_ellie@hotmail.com>
2023-07-28 13:27:04 +0100

    fix: removes old  module syntax & makes rest parameter docs clearer

--- modern_javascript_exercises/rest_parameters/rest_parameters.js
+++ modern_javascript_exercises/rest_parameters/rest_parameters.js
@@ -1,10 +1,9 @@
 // Instructions can be found in rest_parameters.md
 
-function add(a, b) {
+// Add rest parameters here!
+export function add(a, b) {
   // Add a loop here
   return a + b;
 }
 
 add(1, 2, 3, 4, 5);
-
-module.exports = add;
