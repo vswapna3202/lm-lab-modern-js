34d1f2aa0e655b8736d265489497b25b09ea4a01
Swapna V <vswapna3202@gmail.com>
2023-10-09 14:28:52 +0100

    feat: Added code destructuring_arrays

--- modern_javascript_exercises/destructuring_arrays/destructuring_arrays.js
+++ modern_javascript_exercises/destructuring_arrays/destructuring_arrays.js
@@ -1,9 +1,13 @@
 // Instructions can be found in destructuring_arrays.md
-
+/*
 export function sequence(order) {
   const first = order[0];
   const second = order[1];
   const third = order[2];
 
   return first + ", " + second + ", " + third;
+}
+*/
+export const sequence = (order) => {
+  return `${order[0]}, ${order[1]}, ${order[2]}`
 }
