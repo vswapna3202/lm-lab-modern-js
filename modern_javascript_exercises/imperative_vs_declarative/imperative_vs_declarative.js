4a8d4c4d9298951b99aada0109e99fd698d2ed6e
Swapna V <vswapna3202@gmail.com>
2023-10-09 15:16:18 +0100

    feat: added code

--- modern_javascript_exercises/imperative_vs_declarative/imperative_vs_declarative.js
+++ modern_javascript_exercises/imperative_vs_declarative/imperative_vs_declarative.js
@@ -1,13 +1,16 @@
 // Instructions can be found in imperative_vs_declarative.md
 
 export function longPasswords(passwords) {
-  let longPasswords = [];
+  //let longPasswords = [];
 
+  let longPasswords = passwords.filter(password => password.length >= 9);
+
+  /*
   for (let i = 0; i < passwords.length; i++) {
     const password = passwords[i];
     if (password.length >= 9) {
       longPasswords.push(password);
     }
-  }
+  }*/
   return longPasswords;
 }
