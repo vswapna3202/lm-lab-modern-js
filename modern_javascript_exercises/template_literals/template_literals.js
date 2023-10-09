e41469a6376045a8f6b22dfc8df0a57bcbc046eb
Swapna V <vswapna3202@gmail.com>
2023-10-09 15:04:37 +0100

    feat: added code for template_literals

--- modern_javascript_exercises/template_literals/template_literals.js
+++ modern_javascript_exercises/template_literals/template_literals.js
@@ -1,4 +1,5 @@
 // Instructions can be found in template_literals.md
 
 export const greeting = (name, dayOfTheWeek) =>
-  "Hello " + name + "! It is now " + dayOfTheWeek;
+//  "Hello " + name + "! It is now " + dayOfTheWeek;
+  `Hello ${name}! It is now ${dayOfTheWeek}`;
