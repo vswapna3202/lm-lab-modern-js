b274de0773aa4a25d5ac2a6f0cd3421d2e0de48f
Swapna V <vswapna3202@gmail.com>
2023-10-09 14:49:23 +0100

    feat: added code for destructuring_objects

--- modern_javascript_exercises/destructuring_objects/destructuring_objects.js
+++ modern_javascript_exercises/destructuring_objects/destructuring_objects.js
@@ -1,9 +1,13 @@
 // Instructions can be found in destructuring_objects.md
-
+/*
 export function introduction(person) {
   const name = person.name;
   const city = person.city;
   const pet = person.pet;
 
   return "Hello my name is " + name + " and I live in " + city + " with " + pet;
+}*/
+
+export const introduction = ({name, city, pet}) => {
+  return `Hello my name is ${name} and I live in ${city} with ${pet}`;
 }
