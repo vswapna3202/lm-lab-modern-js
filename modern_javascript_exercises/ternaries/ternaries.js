956c20fe0135ac3d1fbda95868996f39415214bd
Swapna V <vswapna3202@gmail.com>
2023-10-09 16:28:30 +0100

    feat: refactored code in ternaries

--- modern_javascript_exercises/ternaries/ternaries.js
+++ modern_javascript_exercises/ternaries/ternaries.js
@@ -1,6 +1,6 @@
 // Instructions can be found in ternaries.md
 
-export function sayHello(name) {
+export const sayHello = (name) => {
   /*if (name === undefined) {
     return "Hello you!";
   } else return "Hello, " + name + "!";*/
