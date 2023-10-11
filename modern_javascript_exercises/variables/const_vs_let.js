1c244e1a59db6031e2a30d44b8d846e4abfe6c7d
Swapna V <vswapna3202@gmail.com>
2023-10-11 21:32:55 +0100

    Feat: Changed const and let correctly

--- modern_javascript_exercises/variables/const_vs_let.js
+++ modern_javascript_exercises/variables/const_vs_let.js
@@ -1,12 +1,12 @@
 // Instructions can be found in const_vs_let.md
 
 function countThePeople() {
-  const count = 0;
-  let people = ["Barbara", "Jacob", "Angela", "Brangela"];
+  let count = 0;
+  const people = ["Barbara", "Jacob", "Angela", "Brangela"];
   people.forEach(() => (count += 1));
   return count;
 }
 
-// console.log(countThePeople()); // <---- uncomment me!
+ console.log(countThePeople()); // <---- uncomment me!
 
 
